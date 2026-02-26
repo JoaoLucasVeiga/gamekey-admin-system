9b61f175f0aac0adc86ba30fc6cffeee77085155
João Lucas Veiga de Carvalho <joaolucasveigadecarvalho@gmail.com>
2026-02-26 12:21:39 -0300

    Unificando conexao localhost e infinityfree

--- config/database.php
+++ config/database.php
@@ -1,26 +1,27 @@
 <?php
-/*******************************************************************************
- * Curso: Engenharia de Software
- * Disciplina: Linguagem e Técnicas de Programação
- * Professor: Flores
- * Turma: ESOFT-2B
- * Componentes:
- *     25291090-2 - João Lucas Veiga de Carvalho
- *     25164719-2 - Rafael Maia
- *     25033056-2 - Gabriel Michels Cubas
- *     25001118-2 - Emanuel Gomes de Almeida
- *     25361613-2 - Arthur Marinho Figueira
- *     25362639-2 - Roberti Mendes de moura
- * Descritivo: Arquivo de configuração para conexão com o banco de dados.
- ******************************************************************************/
 
 function conectar_db() {
-    $host = 'localhost';
-    $dbname = 'db_steamkeys_globais';
-    $user = 'root';
-    $password = '';
+   
+    
+    
+    $whitelist_local = array('127.0.0.1', '::1', 'localhost');
+
+    if (in_array($_SERVER['SERVER_NAME'], $whitelist_local)) {
+        // --- AMBIENTE LOCAL (XAMPP) ---
+        $host = 'localhost';
+        $dbname = 'db_steamkeys_globais'; 
+        $user = 'root';
+        $password = '';
+    } else {
+      
+        $host = 'sql305.infinityfree.com';
+        $dbname = 'if0_41206744_gamekeys'; 
+        $user = 'if0_41206744';
+        $password = 'HCyQdWKO9Rq2';
+    }
 
     try {
+      
         $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $password);
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         return $pdo;
